e37d75afbf2178cd094b37f2e2503ea651294669
Vladyslav Duran <justez4rtz@gmail.com>
2023-04-11 17:33:55 +0300

    build: rest installed, api app created and installed

--- Foodify/settings.py
+++ Foodify/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 
+from Foodify_API.apps import FoodifyApiConfig
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -37,6 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'FoodifyApiConfig',
 ]
 
 MIDDLEWARE = [
