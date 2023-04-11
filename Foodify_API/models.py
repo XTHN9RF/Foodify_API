325cf510ccf2d6e5a6ddec3d4b695e20e1173adc
Vladyslav Duran <justez4rtz@gmail.com>
2023-04-11 18:24:38 +0300

    feat(product model): creat database product model

--- Foodify_API/models.py
+++ Foodify_API/models.py
@@ -29,3 +29,15 @@ class Category(models.Model):
     def __str__(self):
         """" Return string representation of category to display it understandably in the admin panel """
         return self.name
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+    REQUIRED_FIELDS = ['name', 'description', 'price', 'category']
+
+    def __str__(self):
+        return self.name
