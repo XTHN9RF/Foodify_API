f10f91641b10c52dd0c000e68c18fa0ad47f9e55
Vladyslav Duran <justez4rtz@gmail.com>
2023-04-11 17:53:51 +0300

    feat(user): creat user database model

--- Foodify_API/models.py
+++ Foodify_API/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
 
-# Create your models here.
+
+class User(AbstractBaseUser, PermissionsMixin):
+    """Database model that describes users in the system"""
+    email = models.EmailField(max_length=255, unique=True)
+    name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    password = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True)
+    is_admin = models.BooleanField(default=False)
+
+    USERNAME_FIELD = 'email'
+
+    def __str__(self):
+        return self.email
