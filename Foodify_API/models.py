e6b4f96a5f26b4f0c6b4ee700e8a6224139a44af
Vladyslav Duran <justez4rtz@gmail.com>
2023-04-11 19:02:45 +0300

    feat(order model): create database model for orders

--- Foodify_API/models.py
+++ Foodify_API/models.py
@@ -65,3 +65,13 @@ class CartItem(models.Model):
     quantity = models.DecimalField(max_digits=10, decimal_places=2)
 
     REQUIRED_FIELDS = ['product', 'quantity', 'user']
+
+
+class Order(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    address = models.ForeignKey(Address, on_delete=models.CASCADE)
+    cart_items = models.ManyToManyField(CartItem)
+    date = models.DateTimeField(auto_now_add=True)
+    total_price = models.DecimalField(max_digits=10, decimal_places=2)
+
+    REQUIRED_FIELDS = ['user', 'address', 'cart_items', 'date', 'status']
