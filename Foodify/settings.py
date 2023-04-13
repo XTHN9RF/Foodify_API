f4f73f0c6dfe7ffd01955271f7ed9d058e33dba5
Vladyslav Duran <justez4rtz@gmail.com>
2023-04-13 13:05:28 +0300

    fix(installed_app): install app corectly into settings file

--- Foodify/settings.py
+++ Foodify/settings.py
@@ -12,7 +12,6 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 
-from Foodify_API.apps import FoodifyApiConfig
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -40,7 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'FoodifyApiConfig',
+    'rest_framework.authtoken',
+    'Foodify_API',
 ]
 
 MIDDLEWARE = [
@@ -122,7 +122,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-AUTH_USER_MODEL = 'FoodifyApiConfig.User'
+AUTH_USER_MODEL = 'Foodify_API.User'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
