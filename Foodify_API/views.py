500568bc441ad7c2f918f673a9fb68063cddbdb2
Vladyslav Duran <justez4rtz@gmail.com>
2023-04-19 15:42:16 +0300

    feat(category): implement search by name feature into category api view

--- Foodify_API/views.py
+++ Foodify_API/views.py
@@ -1,6 +1,6 @@
 from rest_framework import status
 from rest_framework.response import Response
-from rest_framework import viewsets
+from rest_framework import viewsets, filters
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.settings import api_settings
@@ -25,3 +25,5 @@ class CategoryViewSet(viewsets.ModelViewSet):
     serializer_class = serializers.CategorySerializer
     queryset = models.Category.objects.all()
     permission_classes = (permissions.IsReadOnly,)
+    filter_backends = (filters.SearchFilter,)
+    search_fields = ('name',)
