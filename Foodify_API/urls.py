1fee5a5d50eca3b55f77aa1683b622ca351b4e11
Vladyslav Duran <justez4rtz@gmail.com>
2023-04-22 19:37:11 +0300

    feat(product_urls): add url path to search by category or get list of products

--- Foodify_API/urls.py
+++ Foodify_API/urls.py
@@ -7,8 +7,10 @@ from Foodify_API import views
 router = DefaultRouter()
 router.register('users', views.UserViewSet)
 router.register('categories', views.CategoryViewSet)
+router.register('products', views.ProductViewSet)
 
 urlpatterns = [
     path('login/', views.LoginApiView.as_view()),
+    path('products/<str:pk>/', views.ProductViewSet.as_view({'get': 'list'}), name='product-list'),
     path('', include(router.urls)),
 ]
